39c12fee841dea1dbaf56ab7417ad565308de0af
Yuseeeee <49379138@est.ort.edu.ar>
2025-04-09 09:05:08 -0300

    Update batallanaval.py

--- batallanaval.py
+++ batallanaval.py
@@ -1,5 +1,16 @@
-shlomo = 1234567890
-dani = shlomo / 2
-aieka = 1/138284
-ioni = shlomo * dani * aieka
-print (ioni)
+# Creamos un tablero 10x10 lleno de ceros (o el valor que quieras)
+n = 10
+
+tablero = [[0 for _ in range(n)] for _ in range(n)]
+cantidad_barcos =   10
+disparos = 15
+# Mostramos el tablero
+import random
+columnarandom = random.randint(0,n-1)
+filarandom = random.randint(0,n-1)
+tablero [columnarandom][filarandom] = 1
+for fila in tablero:
+    print(" ".join(str(x) for x in fila)) 
+print(fila)
+print(columnarandom)
+print(filarandom)
